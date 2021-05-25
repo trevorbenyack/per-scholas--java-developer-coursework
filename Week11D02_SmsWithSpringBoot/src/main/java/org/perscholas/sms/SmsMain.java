2a7b84a5176f6fc03529bc4d7abbc71d97e09c59
Trevor Benyack <trevorbenyack@gmail.com>
2021-05-25 14:58:07 -0400

    initial commit for W11D02 SMS w/ Spring Boot app

---
+++ Week11D02_SmsWithSpringBoot/src/main/java/org/perscholas/sms/SmsMain.java
@@ -0,0 +1,13 @@
+package org.perscholas.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsMain.class, args);
+    }
+
+}
